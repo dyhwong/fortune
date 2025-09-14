48d122dd93c804f1567463b212e428c5d453b37a
David Wong <6326445+dyhwong@users.noreply.github.com>
2025-09-14 00:22:32 -0400

    Refactor fortune generation script

--- script.js
+++ script.js
@@ -1,41 +1,36 @@
 addEventListener("load",app);
-function app() {
-	class Fortune {
-		constructor(fortuneList) {
-			this.text = !fortuneList ? "No fortune" : fortuneList[~~(Math.random() * fortuneList.length)];
-		}
-	}
 
+const fortuneList = [
+  "Did you know that fortune cookies aren’t even Chinese? They’re made by Americans, based on a Japanese recipe.",
+  "You have an ability to sense and know higher truth.",
+  "Excellent time to become a missing person.",
+  "Don’t look back, the lemmings are gaining on you.",
+  "You look tired.",
+  "Chess tonight.",
+  "Don’t feed the bats tonight.",
+  "Your boss is a few sandwiches short of a picnic.",
+  "You will forget that you ever knew me.",
+  "Future looks spotty. You will spill soup in late evening.",
+  "You have the capacity to learn from mistakes. You’ll learn a lot today.",
+  "A vivid and creative mind characterizes you.",
+  "If you think last Tuesday was a drag, wait till you see what happens tomorrow!",
+  "You will be attacked next Wednesday at 3:15 P.M. by six samurai sword wielding purple fish glued to Harley-Davidson motorcycles.",
+  "There was a phone call for you.",
+  "Are you making all this up as you go along?",
+  "Don’t plan any hasty moves. You’ll be evicted soon anyway.",
+  "Advancement in position.",
+  "You’re ugly and your mother dresses you funny.",
+  "Be careful! Is it classified?"
+];
+
+function getFortune(fortunes) {
+  return fortunes[~~(Math.random() * fortunes.length)];
+}
+
+function app() {
 	var fcBtn = document.querySelector("button"),
 		fortuneText = document.querySelector(".fc-fortune-text"),
-		fortuneList = [
-			"Did you know that fortune cookies aren’t even Chinese? They’re made by Americans, based on a Japanese recipe.",
-			"You have an ability to sense and know higher truth.",
-			"Excellent time to become a missing person.",
-			"Don’t look back, the lemmings are gaining on you.",
-			"You look tired.",
-			"Chess tonight.",
-			"Don’t feed the bats tonight.",
-			"Your boss is a few sandwiches short of a picnic.",
-			"You will forget that you ever knew me.",
-			"Future looks spotty. You will spill soup in late evening.",
-			"You have the capacity to learn from mistakes. You’ll learn a lot today.",
-			"A vivid and creative mind characterizes you.",
-			"If you think last Tuesday was a drag, wait till you see what happens tomorrow!",
-			"You will be attacked next Wednesday at 3:15 P.M. by six samurai sword wielding purple fish glued to Harley-Davidson motorcycles.",
-			"There was a phone call for you.",
-			"Are you making all this up as you go along?",
-			"Don’t plan any hasty moves. You’ll be evicted soon anyway.",
-			"Advancement in position.",
-			"You’re ugly and your mother dresses you funny.",
-			"Be careful! Is it classified?"
-		],
-		fortune = new Fortune(),
 
-		getFortune = function(){
-			fortune = new Fortune(fortuneList);
-			fortuneText.innerHTML = fortune.text;
-		},
 		nextState = function(){
 			let elClass = this.classList,
 				spawned = "spawned",
@@ -50,10 +45,10 @@ function app() {
 			} else {
 				elClass.remove(opened);
 				elClass.add(spawned);
-				getFortune();
+				fortuneText.innerHTML = getFortune(fortuneList);
 			}
 		};
 
-	getFortune();
+	fortuneText.innerHTML = getFortune(fortuneList);
 	fcBtn.addEventListener("click",nextState);
 }
